7a23e85fd9c1d81d15af766201781142f162273e
root <root@elastic-colden.67-217-245-127.plesk.page>
2024-11-19 20:57:20 +0000

    vpstest

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -14,7 +14,7 @@ export interface currentUser {
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = "http://localhost:5020/api";
+  private apiUrl = "http://67.217.245.127:5001/api";
   private currentUserSubject: BehaviorSubject<currentUser>;
   public currentUser: Observable<currentUser>;
 
