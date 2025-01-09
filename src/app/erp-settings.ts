68e9bf701f5030401502e85e61eac0784efc8881
GustavoAngel2 <124698903+GustavoAngel2@users.noreply.github.com>
2025-01-09 15:52:40 -0600

    -
    -

--- src/app/erp-settings.ts
+++ src/app/erp-settings.ts
@@ -1,60 +1,76 @@
 import { Injectable } from "@angular/core";
 
 @Injectable({
-    providedIn: "root",
+  providedIn: 'root',
 })
+export class ERP {
+  public apiUrl = 'http://67.217.245.127:5001/api';
 
-export class ERP{
+  public paginator = [10, 25, 100];
 
-    public apiUrl = "http://localhost:5020/api";
+  setStyle(theme: string) {
+    const root = document.documentElement.style;
 
-    public paginator = [10, 25, 100]
+    root.setProperty(
+      '--mat-toolbar-container-background-color',
+      'var(--' + theme + ')'
+    );
+    root.setProperty(
+      '--mat-expansion-container-background-color',
+      'var(--' + theme + ')'
+    );
+    root.setProperty(
+      '--mat-sidenav-container-background-color',
+      'var(--dark-' + theme + ')'
+    );
+    root.setProperty(
+      '--mat-paginator-container-background-color',
+      'var(--' + theme + ')'
+    );
+    root.setProperty('--table-color', 'var(--ligth-' + theme + ')');
+    root.setProperty('--header-color', 'var(--' + theme + ')');
+  }
 
-    setStyle(theme:string){
-        const root = document.documentElement.style;
+  setBgColor(bgColor: string, theme: string) {
+    const root = document.documentElement.style;
 
-        root.setProperty('--mat-toolbar-container-background-color', ('var(--' + theme + ')'));
-        root.setProperty('--mat-expansion-container-background-color', ('var(--' + theme + ')'));
-        root.setProperty('--mat-sidenav-container-background-color',('var(--dark-' + theme + ')'));
-        root.setProperty('--mat-paginator-container-background-color',('var(--' + theme + ')'));
-        root.setProperty('--table-color',('var(--ligth-' + theme + ')'))
-        root.setProperty('--header-color',('var(--' + theme + ')'))
+    if (bgColor == 'light') {
+      root.setProperty('--mat-sidenav-content-background-color', '#fff');
+      root.setProperty('--title-color', '#000');
+    } else if (bgColor == 'dark') {
+      root.setProperty('--mat-sidenav-content-background-color', '#353535');
+      root.setProperty('--title-color', '#fff');
+    } else {
+      root.setProperty(
+        '--mat-sidenav-content-background-color',
+        'var(--dark-' + theme + ')'
+      );
+      root.setProperty('--title-color', '#fff');
     }
+  }
 
-    setBgColor(bgColor:string,theme:string){
-        const root = document.documentElement.style
+  saveSettings(theme: string, homeStyle: string, bgColor: string) {
+    localStorage.setItem('color', theme);
+    localStorage.setItem('homeStyle', homeStyle);
+    localStorage.setItem('bgColor', bgColor);
+  }
 
-        if(bgColor == 'light'){
-            root.setProperty('--mat-sidenav-content-background-color', '#fff')
-            root.setProperty('--title-color','#000')
-        } else if (bgColor == 'dark'){
-            root.setProperty('--mat-sidenav-content-background-color', '#353535')
-            root.setProperty('--title-color','#fff')
-        } else {
-            root.setProperty('--mat-sidenav-content-background-color', ('var(--dark-' + theme + ')'))
-            root.setProperty('--title-color','#fff')
-        }
-    }
+  getSettings() {
+    const settings = {
+      color: localStorage.getItem('color') ?? 'blue',
+      homeStyle: localStorage.getItem('homeStyle') ?? 'group',
+      bgColor: localStorage.getItem('bgColor') ?? 'light',
+      pictureStyle: localStorage.getItem('pictureStyle') ?? 'square',
+    };
 
-    saveSettings(theme:string, homeStyle:string,bgColor:string){
-        localStorage.setItem('color', theme);
-        localStorage.setItem('homeStyle', homeStyle);
-        localStorage.setItem('bgColor', bgColor)
-    }
+    return settings;
+  }
 
-    getSettings(){
-        const settings = {
-            color: localStorage.getItem('color') ?? 'blue',
-            homeStyle: localStorage.getItem('homeStyle') ?? 'group',
-            bgColor: localStorage.getItem('bgColor') ?? 'light',
-            pictureStyle: localStorage.getItem('pictureStyle') ?? 'square'
-        }
-
-        return settings
-    }
-
-    loadSettings(){
-        this.setStyle(localStorage.getItem('color') ?? 'blue')
-        this.setBgColor(localStorage.getItem('bgColor') ?? 'light',localStorage.getItem('color') ?? 'blue')
-    }
+  loadSettings() {
+    this.setStyle(localStorage.getItem('color') ?? 'blue');
+    this.setBgColor(
+      localStorage.getItem('bgColor') ?? 'light',
+      localStorage.getItem('color') ?? 'blue'
+    );
+  }
 }
